73e1b0f2a2c59c26ae0e2566aa9de26143baf6bb
Bryce Knoblauch <3880556+SuomafGamer@users.noreply.github.com>
2025-06-18 23:41:02 -0700

    Updates to data/conversion
    Adds diacritics
    updates conversion
    conversion now keeps proper caret position

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Variables
 var convertTo = 'hiragana';
-var i, tableIndex, tableCount;
 var hiraganaMap = {
     // Special Characters
     ' ': '　',
     ',': '、',
     '.': '。',
     '?': '？',
+    '-': 'ー',
     // The following entries are for conversion purposes
     'yi': 'い',
     'ye': 'いぇ',
@@ -37,30 +37,6 @@ var hiraganaMap = {
     'cho': 'ちょ',
     'chu': 'ちゅ',
     'cha': 'ちゃ',
-    // Dakuten (g)
-    'ga': 'が',
-    'gi': 'ぎ',
-    'gu': 'ぐ',
-    'ge': 'げ',
-    'go': 'ご',
-    // Dakuten (z)
-    'za': 'ざ',
-    'zi': 'じ',
-    'zu': 'ず',
-    'ze': 'ぜ',
-    'zo': 'ぞ',
-    // Dakuten (d)
-    'da': 'だ',
-    'di': 'ぢ',
-    'du': 'づ',
-    'de': 'で',
-    'do': 'ど',
-    // Dakuten (b)
-    'ba': 'ば',
-    'bi': 'び',
-    'bu': 'ぶ',
-    'be': 'べ',
-    'bo': 'ぼ',
 };
 var hiraganaArr = [
     // base
@@ -121,10 +97,49 @@ var hiraganaArr = [
     // n
     ['n', 'ん'],
 ];
-
-// Populate the Hiragana Map
+// Add to the Hiragana Map
 for (var i = 0; i < hiraganaArr.length; i++) {
     var parts = hiraganaArr[i];
+    // Map Romaji to Hiragana
+    hiraganaMap[parts[0]] = parts[1];
+    // Map Hiragana to Romaji
+    hiraganaMap[parts[1]] = parts[0];
+}
+var hiraganaDiacritics = [
+    // Dakuten (g)
+    ['ga', 'が'],
+    ['gi', 'ぎ'],
+    ['gu', 'ぐ'],
+    ['ge', 'げ'],
+    ['go', 'ご'],
+    // Dakuten (z)
+    ['za', 'ざ'],
+    ['zi', 'じ'],
+    ['zu', 'ず'],
+    ['ze', 'ぜ'],
+    ['zo', 'ぞ'],
+    // Dakuten (d)
+    ['da', 'だ'],
+    ['di', 'ぢ'],
+    ['du', 'づ'],
+    ['de', 'で'],
+    ['do', 'ど'],
+    // Dakuten (b)
+    ['ba', 'ば'],
+    ['bi', 'び'],
+    ['bu', 'ぶ'],
+    ['be', 'べ'],
+    ['bo', 'ぼ'],
+    // Handakuten (p)
+    ['pa', 'ぱ'],
+    ['pi', 'ぴ'],
+    ['pu', 'ぷ'],
+    ['pe', 'ぺ'],
+    ['po', 'ぽ'],
+];
+// Add to the Hiragana Map
+for (var i = 0; i < hiraganaDiacritics.length; i++) {
+    var parts = hiraganaDiacritics[i];
     // Map Romaji to Hiragana
     hiraganaMap[parts[0]] = parts[1];
     // Map Hiragana to Romaji
@@ -137,6 +152,7 @@ var katakanaMap = {
     ',': '、',
     '.': '。',
     '?': '？',
+    '-': 'ー',
     // The following entries are for conversion purposes
     'yi': 'イ',
     'ye': 'イェ',
@@ -167,30 +183,6 @@ var katakanaMap = {
     'cho': 'チョ',
     'chu': 'チュ',
     'cha': 'チャ',
-    // Dakuten (g)
-    'ga': 'ガ',
-    'gi': 'ギ',
-    'gu': 'グ',
-    'ge': 'ゲ',
-    'go': 'ゴ',
-    // Dakuten (z)
-    'za': 'ザ',
-    'zi': 'ジ',
-    'zu': 'ズ',
-    'ze': 'ゼ',
-    'zo': 'ゾ',
-    // Dakuten (d)
-    'da': 'ダ',
-    'di': 'ヂ',
-    'du': 'ヅ',
-    'de': 'デ',
-    'do': 'ド',
-    // Dakuten (b)
-    'ba': 'バ',
-    'bi': 'ビ',
-    'bu': 'ブ',
-    'be': 'ベ',
-    'bo': 'ボ',
 };
 var katakanaArr = [
     // base
@@ -236,9 +228,9 @@ var katakanaArr = [
     ['me', 'メ'],
     ['mo', 'モ'],
     // y
-    ['ya', 'ヤ'],
-    ['yu', 'ユ'],
-    ['yo', 'ヨ'],
+    ['ya', 'ヤ'], // 35
+    ['yu', 'ユ'], // 37
+    ['yo', 'ヨ'], // 39
     // r
     ['ra', 'ラ'],
     ['ri', 'リ'],
@@ -246,15 +238,54 @@ var katakanaArr = [
     ['re', 'レ'],
     ['ro', 'ロ'],
     // w
-    ['wa', 'ワ'],
-    ['wo', 'ヲ'],
+    ['wa', 'ワ'], // 45
+    ['wo', 'ヲ'], // 49
     // n
-    ['n', 'ン'],
+    ['n', 'ン'],  // 50
 ];
-
 // Populate the Katakana Map
-for (i = 0; i < katakanaArr.length; i++) {
+for (var i = 0; i < katakanaArr.length; i++) {
     var parts = katakanaArr[i];
+    // Map Romaji to Katakana
+    katakanaMap[parts[0]] = parts[1];
+    // Map Katakana to Romaji
+    katakanaMap[parts[1]] = parts[0];
+}
+var katakanaDiacritics = [
+    // Dakuten (g)
+    ['ga', 'ガ'],
+    ['gi', 'ギ'],
+    ['gu', 'グ'],
+    ['ge', 'ゲ'],
+    ['go', 'ゴ'],
+    // Dakuten (z)
+    ['za', 'ザ'],
+    ['zi', 'ジ'],
+    ['zu', 'ズ'],
+    ['ze', 'ゼ'],
+    ['zo', 'ゾ'],
+    // Dakuten (d)
+    ['da', 'ダ'],
+    ['di', 'ヂ'],
+    ['du', 'ヅ'],
+    ['de', 'デ'],
+    ['do', 'ド'],
+    // Dakuten (b)
+    ['ba', 'バ'],
+    ['bi', 'ビ'],
+    ['bu', 'ブ'],
+    ['be', 'ベ'],
+    ['bo', 'ボ'],
+    // Handakuten (p)
+    ['pa', 'パ'],
+    ['pi', 'ピ'],
+    ['pu', 'プ'],
+    ['pe', 'ペ'],
+    ['po', 'ポ'],
+];
+// Populate the Katakana Map
+for (var i = 0; i < katakanaDiacritics.length; i++) {
+    var parts = katakanaDiacritics[i];
     // Map Romaji to Katakana
     katakanaMap[parts[0]] = parts[1];
     // Map Katakana to Romaji
@@ -262,28 +293,29 @@ for (i = 0; i < katakanaArr.length; i++) {
 }
 
 // Used to print the hiragana/katakana arrays
-var kanaTableArr = [5, 5, 5, 5, 5, 5, 5, 3, 5, 2, 1];
-var kanaTableLength = 5 + 5 + 5 + 5 + 5 + 5 + 5 + 3 + 5 + 2 + 1;
+var kanaTableCellSkip = { 36: true, 38: true, 46: true, 47: true, 48: true };
+var kanaTableLength = 51; // 46 kana + 5 skipped cells
 function printKana(type) {
-    var _html = '<div class="kana-table"><div class="kana-row">';
-    tableIndex = 0;
-    tableCount = 0;
+    var _html = '<table class="kana-table"><tr class="kana-row">';
+    var rowCount = 0;
+    var skip = 0;
     // Initialize Stuff and Things
-    for (i = 0; i < kanaTableLength /*window[`${type}Arr`].length*/; i++) {
-        var parts = window[`${type}Arr`][i];
-        var romaji = parts[0];
-        var kana = parts[1];
-        _html += `<span class="kana" title="${romaji}">${kana}</span>`;
-        // console.log('tableCount, tableIndex', tableCount, tableIndex);
-        if (++tableCount >= kanaTableArr[tableIndex]) {
-            if (tableCount >= kanaTableArr[tableIndex]) {
-                _html += '</div><div class="kana-row">';
-            }
-            tableCount = 0;
-            tableIndex++;
+    for (var i = 0; i < kanaTableLength; i++) {
+        if (kanaTableCellSkip[i]) {
+            _html += `<td class="kana"></td>`;
+            skip++;
+            continue;
+        }
+        var parts = window[`${type}Arr`][i - skip];
+        var romaji = parts && parts[0] || '';
+        var kana = parts && parts[1] || '';
+        _html += `<td class="kana" title="${romaji}">${kana}</td>`;
+        if ((i + 1) % 5 == 0) {
+            _html += '</tr><tr class="kana-row">';
+            rowCount++;
         }
     }
-    _html += '</div></div>';
+    _html += '</tr></table>';
     document.querySelector(`#${type}`).innerHTML = _html;
 }
 
@@ -294,11 +326,15 @@ function init() {
 }
 
 function conversion(event) {
-    console.log("Input:", event.target.value);
+    var textArea = event.target;
+    console.log("Input:", textArea.value);
     var map = window[convertTo + 'Map'];
-    var _text = event.target.value.toLowerCase();
+    var _text = textArea.value.toLowerCase();
+    // To preserve the caret position
+    var startLength = _text.length;
+    var caretPos = textArea.selectionStart;
     // Four Characters (i.e. kkya = っきゃ)
-    _text = _text.replace(/([kstnhmyrgzdb]){2}(y[auo])/g, (fullMatch, letter, y/*, position, entireText*/) => {
+    _text = _text.replace(/([kstnhmyrgzdbp]){2}(y[auo])/g, (fullMatch, letter, y/*, position, entireText*/) => {
         return `っ${map[`${letter}i`]}${map[`${y}`]}`;
     });
     // Three Characters Unique (i.e. shi = し)
@@ -306,11 +342,11 @@ function conversion(event) {
         return map[fullMatch];
     });
     // Three Characters (i.e. kya = きゃ)
-    _text = _text.replace(/([kstnhmyrgzdb])(y[auo])/g, (fullMatch, letter, y/*, position, entireText*/) => {
+    _text = _text.replace(/([kstnhmyrgzdbp])(y[auo])/g, (fullMatch, letter, y/*, position, entireText*/) => {
         return `${map[`${letter}i`]}${map[`${y}`]}`;
     });
     // Three Characters (i.e. kka = っか)
-    _text = _text.replace(/([kstnhmyrgzdb]){2}([aiueo])/g, (fullMatch, letter, vowel/*, position, entireText*/) => {
+    _text = _text.replace(/([kstnhmyrgzdbp]){2}([aiueo])/g, (fullMatch, letter, vowel/*, position, entireText*/) => {
         return `っ${map[`${letter}${vowel}`]}`;
     });
     // Two Characters Unique (i.e. fe = ふぇ)
@@ -318,7 +354,7 @@ function conversion(event) {
         return map[fullMatch];
     });
     // Two Characters (i.e. ka = か)
-    _text = _text.replace(/[kstnhmyrwgzdb][aiueo]/g, (fullMatch/*, position, entireText*/) => {
+    _text = _text.replace(/[kstnhmyrwgzdbp][aiueo]/g, (fullMatch/*, position, entireText*/) => {
         return map[fullMatch];
     });
     // Two Characters (i.e. fu = ふ)
@@ -326,10 +362,16 @@ function conversion(event) {
         return map[fullMatch];
     });
     // One Character (i.e. a = あ)
-    _text = _text.replace(/[aiueo ,\.\?]/g, (fullMatch/*, position, entireText*/) => {
+    _text = _text.replace(/[aiueo ,\.\?-]/g, (fullMatch/*, position, entireText*/) => {
         return map[fullMatch];
     });
+    // To preserve the caret position
+    var endLength = _text.length;
+    var newCaretPos = caretPos - (startLength - endLength);
+    // Overwriting the text moves the caret
     event.target.value = _text;
+    // Set the correct caret position
+    textArea.setSelectionRange(newCaretPos, newCaretPos);
 }
 
 function selectConversion(newConv) {
